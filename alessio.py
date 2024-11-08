d5212aa8dc0dd84959d11720f4472e15f5c4ae5a
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2024-11-08 15:30:02 +0100

    Update alessio.py

---
+++ alessio.py
@@ -0,0 +1,51 @@
+class Utente:
+    
+    attributi: username, passw, punteggio=0
+
+    #getter setter utility..
+
+
+class Gioco:
+    # chiamato quando il login effettuato
+    def controlla livello difficolta
+
+    def gioco1
+
+    def gioco2
+
+    def avvia_gioco:
+        while gioco
+        a seconda del livello
+        chiama giocox
+        
+    def aggiorna classifica(classifica)
+
+
+
+class Classifica:
+    dizionario username:punteggio
+
+    def visualizza Classifica
+
+    def salva in File   
+    
+
+    
+
+class Menu:
+    pass
+
+    def start()
+        while attivo
+            print(benvenuto)
+            # chiede login
+            # quando l'utente è entrato inizia gioco
+            gioco = Gioco()
+
+
+        # salva classifica
+
+    def login()
+        
+    def registrazione()
+        # crea oggetto Utente
